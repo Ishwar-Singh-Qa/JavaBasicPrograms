ce681c2ba52d44506afa3c92a9f217ba9b2cf98e
Ishwar-Singh-Qa <qa.ishwar16@gmail.com>
2024-09-23 19:31:39 -0700

    my first commit

---
+++ For_Loop_Incremental.java
@@ -0,0 +1,15 @@
+//To Repeat the code.Print the counting from 1 to 10
+
+package loops_and_if_statements;
+
+public class For_Loop_Incremental {
+
+	public static void main(String[] args) 
+	{
+		for (int i=1; i<=10; i++)              //RESULT WILL BE SAME IF WRITE ++i also (almost in every case of For loop)
+		{
+			System.out.println("Hey, How have you been?");
+		}
+	}
+
+}
